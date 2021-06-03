815b9783dda5086675be27eb6ea04e0148d3d756
iahunter <travisriesenberg@gmail.com>
2021-06-03 16:13:16 -0500

    Update NetworkDiag1.ps1

--- NetworkDiag1.ps1
+++ NetworkDiag1.ps1
@@ -1,10 +1,15 @@
 # Script to run on User machine during intermitten issues. Will Do Network Diagnostic commands and output them into a file Named ProblemLog.txt in the directory where you run the script
 # Run in Powershell CLI by executing .\NetworkDiag1.ps1
 
-Write-Output "###################################"
-Write-Output "  Starting Network Diagnostic Log  "
-Write-Output "###################################"
+$timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
 
+Write-Output "###################################" | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
+Write-Output "  Starting Network Diagnostic Log  " | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
+Write-Output "###################################" | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
+
+Write-Output $timestamp | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
+
+# Domains we want to querie for DNS Checks
 $nslookups = @(
                 "sip.pstnhub.microsoft.com.com",
                 "google.com",
@@ -12,52 +17,52 @@ $nslookups = @(
                 "apple.com"
               )
 
-
+# Domains and IPs we want to check connectivity to
 $pings = @("google.com",
             "4.2.2.2",
             "8.8.8.8")
 
-
+# Loop until we cancel
 while(1){
 
     $date = Get-Date 
-
+    
     # Record Date and Time
-    Write-Output $date | Tee-Object -FilePath .\ProblemLog.txt -Append
+    Write-Output $date | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
     
     # IP Configuration
-    ipconfig /all | Tee-Object -FilePath .\ProblemLog.txt -Append
+    ipconfig /all | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
     # Wifi Diagnostics
-    netsh wlan show interface | Tee-Object -FilePath .\ProblemLog.txt -Append
+    netsh wlan show interface | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
     # DNS Checks
     ForEach ($nslookup in $nslookups) {
         # Use Default DNS Server
         Write-Output "nslookup $nslookup"
         $result = nslookup $nslookup
-        $result | Tee-Object -FilePath .\ProblemLog.txt -Append
+        $result | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
         # Use Static External DNS Server
         Write-Output "nslookup $nslookup 1.1.1.1"
         $result = nslookup $nslookup 1.1.1.1
-        $result | Tee-Object -FilePath .\ProblemLog.txt -Append
+        $result | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
         # Use Static External DNS Server
         Write-Output "nslookup $nslookup 8.8.8.8"
         $result = nslookup $nslookup 8.8.8.8
-        $result | Tee-Object -FilePath .\ProblemLog.txt -Append
+        $result | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
     }
 
     # IPv4 Internet Reachablity Checks
     ForEach ($ping in $pings) {
         Write-Output "ping $ping"
         $result = ping $ping
-        $result | Tee-Object -FilePath .\ProblemLog.txt -Append
+        $result |Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
         Write-Output "tracert $ping"
         $result = tracert $ping
-        $result | Tee-Object -FilePath .\ProblemLog.txt -Append
+        $result |Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
 
     }
 
