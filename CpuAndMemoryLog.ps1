4630f71515421c946649f9cfa4b0d1b365abe4ee
iahunter <travisriesenberg@gmail.com>
2021-06-18 12:43:23 -0500

    Update CpuAndMemoryLog.ps1

--- CpuAndMemoryLog.ps1
+++ CpuAndMemoryLog.ps1
@@ -1,3 +1,6 @@
+# To run on a remote machine. 
+# Enter-PSSession -ComputerName <name> -Credential <creds>
+
 $timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
 
 Write-Output "###################################" | Tee-Object -FilePath .\ProblemLog-$timestamp.txt -Append
